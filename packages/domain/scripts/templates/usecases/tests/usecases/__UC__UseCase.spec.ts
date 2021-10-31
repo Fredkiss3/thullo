97653a7a201e3436e0db4361e3fb3d87300c8f88
fredhel KISSIE <fredkiss3gmail.com>
2021-10-31 04:55:48 +0100

    fix: refactoring

--- packages/domain/scripts/templates/usecases/tests/usecases/__UC__UseCase.spec.ts
+++ packages/domain/scripts/templates/usecases/tests/usecases/__UC__UseCase.spec.ts
@@ -1,43 +1,26 @@
 import {
-  __UC__Request,
-  __UC__UseCase,
-  __UC__Presenter,
-  __UC__Response,
+    __UC__Request,
+    __UC__UseCase,
+    __UC__Presenter,
+    __UC__Response
 } from '@thullo/domain';
 
 const presenter = new (class implements __UC__Presenter {
-  response?: __UC__Response | null;
+    response?: __UC__Response | null;
 
-  present(response: __UC__Response): void {
-    this.response = response;
-  }
+    present(response: __UC__Response): void {
+        this.response = response;
+    }
 })();
 
+const request: __UC__Request = {};
+
 describe('__UC__ Use case', () => {
-  beforeEach(() => {
-    presenter.response = null;
-  });
+    beforeEach(() => {
+        presenter.response = null;
+    });
 
-  it('is successful', async () => {
-    // Given
-    const useCase = new __UC__UseCase();
-    const request: __UC__Request = {};
-
-    // When
-    await useCase.execute(request, presenter);
-
-    // Then
-    expect(presenter.response).not.toBe(null);
-  });
-
-  it.todo('Negative Use Case');
-
-  describe('Invalid Requests', () => {
-    const dataset: { label: string; request: __UC__Request }[] = [];
-
-    it.each(dataset)(
-      'shows errors with invalid request : "$label"',
-      async ({ request }) => {
+    it('is successful', async () => {
         // Given
         const useCase = new __UC__UseCase();
 
@@ -45,8 +28,34 @@ describe('__UC__ Use case', () => {
         await useCase.execute(request, presenter);
 
         // Then
-        expect(presenter.response?.errors).not.toBe(null);
-      }
-    );
-  });
+        expect(presenter.response).not.toBe(null);
+    });
+
+    it.todo('Negative Use Case');
+
+    describe('Invalid Requests', () => {
+        const dataset: { label: string; request: __UC__Request }[] = [
+            // TODO: Specify requests
+            {
+                label: 'Example label',
+                request: {
+                    ...request
+                }
+            }
+        ];
+
+        it.each(dataset)(
+            'shows errors with invalid request : "$label"',
+            async ({ request }) => {
+                // Given
+                const useCase = new __UC__UseCase();
+
+                // When
+                await useCase.execute(request, presenter);
+
+                // Then
+                expect(presenter.response?.errors).not.toBe(null);
+            }
+        );
+    });
 });
