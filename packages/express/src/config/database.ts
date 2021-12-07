6b4fa6af84568796c0d92b34468aaca681806670
fredhel KISSIE <fredkiss3gmail.com>
2021-12-08 00:39:45 +0100

    fix(express): Removed unused file "database.ts"

--- packages/express/src/config/database.ts
+++
@@ -1,9 +0,0 @@
-export class Database {
-    connect() {
-        console.log('Database connected with environment: ', {
-            DB_HOST: process.env.DB_HOST,
-            DB_PORT: process.env.DB_PORT,
-            DB_NAME: process.env.DB_NAME,
-        });
-    }
-}
