1ac15f80fbdc1f0a926167e989449dd51af60e62
fredhel KISSIE <fredkiss3gmail.com>
2022-02-06 18:53:56 +0100

    fix: Remove the nextjs package because it is not needed

--- packages/next/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/base/reset.scss";
-import "../styles/base/typography.scss";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
