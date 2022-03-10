58703bf9ff91553da81e136818277b6b77a3879c
fredhel KISSIE <fredkiss3gmail.com>
2022-03-11 00:07:45 +0100

    feat(front): Add list to board finished

--- packages/front/src/components/list.tsx
+++ packages/front/src/components/list.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react';
 import { List as ListType } from '@/lib/types';
+import cls from '@/styles/components/list.module.scss';
+import { Icon } from '@/components/icon';
+import { Button } from '@/components/button';
+import { useToastContext } from '@/context/toast.context';
 
 export interface ListProps {
     list: ListType;
@@ -7,9 +10,26 @@ export interface ListProps {
 }
 
 export function List({ list: { cards, name }, className }: ListProps) {
+    const { dispatch } = useToastContext();
     return (
-        <div className={className}>
-            <span>{name}</span>
+        <div className={`${cls.list} ${className ?? ''}`}>
+            <div className={cls.list__header}>
+                <span>{name}</span>
+                <Button
+                    square
+                    renderTrailingIcon={(cls) => (
+                        <Icon icon="h-dots" className={cls} />
+                    )}
+                    onClick={() => {
+                        dispatch({
+                            type: 'ADD_WARNING',
+                            key: 'list',
+                            message: 'This feature is not implemented yet',
+                        });
+                    }}
+                />
+            </div>
+            <div className={cls.list__cards}></div>
         </div>
     );
 }
