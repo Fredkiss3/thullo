e43522a6394dcaaa5f637cd997df13364d46b666
fredhel KISSIE <fredkiss3gmail.com>
2022-03-11 01:43:24 +0100

    fix(api): set unsplash api as public

--- packages/express/src/routes/unsplash.ts
+++ packages/express/src/routes/unsplash.ts
@@ -1,14 +1,14 @@
 import router from 'express-promise-router';
 import { getController } from '../lib/functions';
-import { auth_middleware } from '../middleware/auth';
+// import { auth_middleware } from '../middleware/auth';
 import { UnsplashSearchController } from '../controllers/UnsplashSearchController';
-import { UnsplashListController } from "../controllers/UnsplashListController";
-import { UnsplashRandomController } from "../controllers/UnsplashRandomController";
+import { UnsplashListController } from '../controllers/UnsplashListController';
+import { UnsplashRandomController } from '../controllers/UnsplashRandomController';
 
 const Router = router();
 
-Router.get('/list', auth_middleware, getController(UnsplashListController));
-Router.get('/search', auth_middleware, getController(UnsplashSearchController));
-Router.get('/random', auth_middleware, getController(UnsplashRandomController));
+Router.get('/list', getController(UnsplashListController));
+Router.get('/search', getController(UnsplashSearchController));
+Router.get('/random', getController(UnsplashRandomController));
 
 export { Router as unsplashRouter };
