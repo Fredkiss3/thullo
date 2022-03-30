0aabcf334e9bf57ef494dbcc9b8e78e88fb40a32
fredhel KISSIE <fredkiss3gmail.com>
2022-03-30 17:01:23 +0200

    refactor(front): Update to react version 18

--- packages/front/src/main.tsx
+++ packages/front/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/base/reset.scss';
 import './styles/base/typography.scss';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
