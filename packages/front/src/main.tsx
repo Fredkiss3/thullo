899139b7f13a2bd63bcda2d1b74d6e4029bcc5a3
fredhel KISSIE <fredkiss3gmail.com>
2022-03-30 21:40:03 +0200

    Revert "refactor(front): Update to react version 18"
    This reverts commit 0aabcf334e9bf57ef494dbcc9b8e78e88fb40a32.

--- packages/front/src/main.tsx
+++ packages/front/src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/base/reset.scss';
 import './styles/base/typography.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
