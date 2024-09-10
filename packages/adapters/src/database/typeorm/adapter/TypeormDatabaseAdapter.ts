bb26158809004855416d386fa7104177de239566
Adrien KISSIE <fredkiss3@gmail.com>
2024-09-10 22:56:09 +0200

    ♻️ use separate values for mongoDB config

--- packages/adapters/src/database/typeorm/adapter/TypeormDatabaseAdapter.ts
+++ packages/adapters/src/database/typeorm/adapter/TypeormDatabaseAdapter.ts
@@ -1,8 +1,10 @@
 import { container, singleton } from 'tsyringe';
-import { Connection, createConnection } from 'typeorm';
-import { Database } from '../../database';
+import { type type type type type type Connection, createConnection } from 'typeorm';
+import type type { Database } from '../../database';
 import { BoardEntity, ParticipationEntity } from '../entities/Board';
 import { MemberEntity } from '../entities/Member';
+import type typeimport { Database } from '../../database';
+ { Database } from '../../database';
 
 @singleton()
 export class TypeormDatabaseAdapter implements Database {
@@ -35,6 +37,11 @@ export class TypeormDatabaseAdapter implements Database {
     }
 }
 
+const MONGO_URI = `mongodb://${process.env.MONGO_USER!}:${encodeURIComponent(process.env.MONGO_PASSWORD!)}@${process.env.MONGO_DB_HOST!}:27017/thullo?authSource=admin`
+console.log({
+    MONGO_URI
+})
+
 container.register(TypeormDatabaseAdapter, {
-    useValue: new TypeormDatabaseAdapter(process.env.MONGO_URI!)
+    useValue: new TypeormDatabaseAdapter(MONGO_URI)
 });
